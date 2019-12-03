07f1a1816f2ff79e6b5556e3bbe6bdc295782ab1
beryllium312 <31936889+beryllium312@users.noreply.github.com>
2019-12-03 12:14:18 -0500

    Added a door
    Added trigger door

---
+++ Untitled_Stealth_Game_/Assets/DoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] public bool isOpen = false;
+    [SerializeField] private int timeOpen;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        timeOpen = 4;
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("In DoorKey col");
+            isOpen = true;
+            StartCoroutine(OpenDoor(timeOpen));
+        }
+
+    }
+    private IEnumerator OpenDoor(int openTime_)
+    {
+        yield return new WaitForSeconds(openTime_);
+        isOpen = false;
+    }
+}
---
+++ Untitled_Stealth_Game_/Assets/OpenDoor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] public GameObject openPosition;
+    [SerializeField] public DoorTrigger doorTriggerObj;
+    [SerializeField] private int speed;
+    private Vector3 close;
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.zero;
+        close = transform.position;//starting position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorTriggerObj.isOpen == true) {
+            OpenTheDoor();
+        }
+        if (doorTriggerObj.isOpen == false)
+        {
+            CloseDoor();
+        }
+
+    }
+    private void OpenTheDoor() {
+        direction = openPosition.transform.position - transform.position;        
+        Vector3 velocity = direction.normalized * speed / 5;
+        transform.position += velocity;
+    }
+
+    private void CloseDoor()
+    {
+        direction = close - transform.position;
+        Vector3 velocity = direction.normalized * speed / 5;
+        transform.position += velocity;
+    }
+}
